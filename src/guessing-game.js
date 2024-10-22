940217af2f019a5666a1cc01f9f840880ac98e25
suzurini <valloforr@gmail.com>
2024-10-22 15:32:49 +1000

    feat: add setRange and guess()

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,11 +2,18 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        if(this.min + 1 === number) {
+            return this.min + 1
+        } else if (this.max - 1 === number) {
+            return this.max - 1
+        } else {
+            return Math.round((this.max - this.min) / 2 + this.min)
+        }
     }
 
     lower() {
